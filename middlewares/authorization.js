2fc5267a28ed2b82b618f4e36358707614231080
feri1304 <ferihandoyo1304@gmail.com>
2022-08-09 15:34:08 +0700

    middlewares

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,18 +1,8 @@
-import db from '../../../libs/db';
 import jwt from 'jsonwebtoken';
+
 export default async function handler(req, res){
     if(req.method !== 'GET') return res.status(405).end();
     
-    const {authorization} = req.headers;
-    if(!authorization) return res.status(401).end();
-
-    const authSplit = authorization.split(' ');
-    
-    const [authType, authToken]=[
-        authSplit[0],
-        authSplit[1]
-    ]
-    if (authType!=='bearer') return res.status(401).end();
     try {
         const verify = jwt.verify(authToken, 'akuganteng');
         console.log(verify);
